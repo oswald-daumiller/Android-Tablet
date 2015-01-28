8d109d3f75d817867e5990f88d2bca30686a0ab9
oswald-daumiller <oswald-daumiller@outlook.com>
2015-01-28 16:29:54 +0100

    Create Local repository

---
+++ android-demo/src/com/example/cardflight/Settings.java
@@ -0,0 +1,10 @@
+package com.example.cardflight;
+
+/**
+ * Created by pcedrowski on 9/2/14.
+ */
+public class Settings {
+    //TODO Put your API & Account Tokens here
+    public static final String API_TOKEN = "YOUR_API_TOKEN";
+    public static final String ACCOUNT_TOKEN = "YOUR_ACCOUNT_TOKEN";
+}
